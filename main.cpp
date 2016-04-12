8a59be77c2b89f876c3186ce2cb117e62116cdc9
hasherezade <hasherezade@op.pl>
2016-04-12 10:21:57 +0200

    [FEATURE] Added detecting Petya's presence on the disk

--- main.cpp
+++ main.cpp
@@ -10,8 +10,45 @@
 
 typedef unsigned char BYTE;
 
-
 char out_buf[0x400];
+
+bool check_pattern(FILE *fp, size_t offset, const char *cmp_buf, size_t cmp_size)
+{
+    //cmp_size = (cmp_size > sizeof(out_buf)) ? sizeof(out_buf) : cmp_size;
+
+    fseek(fp, offset, SEEK_SET);
+    size_t read = fread(out_buf, 1, cmp_size, fp);
+
+    if (read != cmp_size) {
+        printf("Error, read = %d\n", read);
+        return false;
+    }
+
+    if (memcmp(out_buf, cmp_buf, cmp_size-1) == 0) {
+        return true;
+    }
+    return false;
+}
+
+bool is_infected(FILE *fp)
+{
+    char Bootloader[] = \
+    "\xfa\x66\x31\xc0\x8e\xd0\x8e\xc0\x8e\xd8\xbc\x00\x7c\xfb\x88\x16"
+    "\x93\x7c\x66\xb8\x20\x00\x00\x00\x66\xbb\x22\x00\x00\x00\xb9\x00"
+    "\x80\xe8\x14\x00\x66\x48\x66\x83\xf8\x00\x75\xf5\x66\xa1\x00\x80"
+    "\xea\x00\x80\x00\x00";
+
+    const size_t bootloader_offset = 0;
+    bool has_bootloader = check_pattern(fp, bootloader_offset, Bootloader, sizeof(Bootloader));
+    if (has_bootloader) printf("[+] Petya bootloader detected!\n");
+
+    char http_pattern[] = "http://";
+    const size_t http_offset = 54 * SECTOR + 0x29;
+    bool has_http = check_pattern(fp, http_offset, http_pattern, sizeof(http_pattern));
+    if (has_http) printf("[+] Petya http address detected!\n");
+
+    return has_bootloader || has_http;
+}
 
 bool decode(BYTE *encoded, BYTE *key) 
 {
@@ -43,7 +80,7 @@ int stage1(FILE *fp)
         return -1;
     }
     if (strlen(outbuf) != 16) {
-        printf("Invalid Stage1 key length!\n");
+        printf("Invalid Stage1 key! Probably the key has been already ereased!\n");
         return -2;
     }
     printf ("Key: %s\n", outbuf);
@@ -95,7 +132,14 @@ int main(int argc, char *argv[])
         printf("Cannot open file\n");
         return -1;
     }
-    
+
+    if (is_infected(fp)) {
+        printf("[+] Petya FOUND on the disk!\n");
+    } else {
+        printf("[-] Petya not found on the disk!\n");
+    }
+    printf("---\n");
+
     if (stage1(fp) == 0) {
         printf("[OK] Stage 1 key recovered!\n");
         fclose(fp);
